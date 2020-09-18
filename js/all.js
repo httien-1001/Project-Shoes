4fb84a4520d9f12f30e0988d5379c0cefd4f804d
httien-1001 <tien.ht187644@sis.hust.edu.vn>
2020-09-18 11:32:42 +0700

    upload item upload carousel

--- js/all.js
+++ js/all.js
@@ -9,4 +9,24 @@ function myFunction() {
   } else {
     header.classList.remove("sticky");
   }
-}
+};
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:7,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:2,
+          nav:true
+      },
+      1000:{
+          items:4,
+          loop:true,
+          // nav:true
+      }
+  }
+})
