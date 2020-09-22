d3e86202bb9c15de8bf9fffa6b75a0b504a18fb8
httien-1001 <tien.ht187644@sis.hust.edu.vn>
2020-09-22 22:35:20 +0700

    change-icon-when-click-menu-mobile

--- js/mobile.js
+++ js/mobile.js
@@ -8,3 +8,21 @@ $("#m-footer-2").click(function(){
 $("#m-footer-3").click(function(){
     $("#m-footer-3-sub").slideToggle();
   });
+$('#click-menu').click(function () { 
+      //alert('hello');
+    isBool = true;
+    if (isBool) {
+        //$('#change-icon').html("<i class='fas fa-time' id='change-icon'></i>");
+        $(this).find($("#change-icon")).removeClass('fa-bars').addClass('fa-times');
+        isBool = false;
+    } else {
+        
+        $('#change-icon').removeClass('fa-times').addClass('fa-bars');
+        
+
+        // $(this).find($("#change-icon")).removeClass('fa-times').addClass('fa-bars');
+        isBool = true;
+        
+    }
+    
+});  
