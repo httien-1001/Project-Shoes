0814ef4221b59760f988181678b0d5ec3058e4e5
httien-1001 <tien.ht187644@sis.hust.edu.vn>
2020-09-22 22:08:05 +0700

    footer-responsive

---
+++ js/mobile.js
@@ -0,0 +1,10 @@
+$("#m-footer-1").click(function(){
+    $("#m-footer-1-sub").slideToggle();
+  });
+
+$("#m-footer-2").click(function(){
+    $("#m-footer-2-sub").slideToggle();
+  });
+$("#m-footer-3").click(function(){
+    $("#m-footer-3-sub").slideToggle();
+  });
